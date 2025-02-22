50ac8d2a906caebba47c404db5f0a2489b3a0031
OMAR MALIK PMP, CCP, ACiarb, PE, MIEAust <95688332+omarmalik347@users.noreply.github.com>
2025-02-22 16:27:39 +0300

    Update safework.py

--- safework.py
+++ safework.py
@@ -14,7 +14,7 @@ st.title("Safe Work Detection Model")
 @st.cache_resource
 def load_model():
     try:
-        model = YOLO("yolo11x-seg.pt")  # Load model
+        model = YOLO("yolo11x-obb.pt")  # Load model
         return model
     except Exception as e:
         st.error(f"Error loading model: {e}")
